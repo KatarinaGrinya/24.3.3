e3c30dfd7e06df48fa04bf2568d0d494101b9d36
KatarinaGrinya <119685916+KatarinaGrinya@users.noreply.github.com>
2023-03-04 08:57:42 +0300

    Update and rename requests.py to req.py

--- req.py
+++ req.py
@@ -25,7 +25,7 @@ headers = {
 res_1 = requests.post(f'https://petstore.swagger.io/v2/pet', headers=headers, data=json.dumps(data))
 
 print(res_1.status_code)
-print(f'\nСоздан питомце "{pet_1}"\nКод ответа - 200\n')
+print(f'Создан питомец "{pet_1}"\nКод ответа - 200\n')
 
 pet_id = dict(res_1.json())['id']
 
@@ -36,28 +36,28 @@ data['name'] = pet_2
 res_2 = requests.put(f'https://petstore.swagger.io/v2/pet', headers=headers, data=json.dumps(data))
 
 print(res_2.status_code)
-print(f'\nИмя питомца "{pet_1}" изменено на "{pet_2}"\nКод ответа - 200')
+print(f'Имя питомца "{pet_1}" изменено на "{pet_2}"\nКод ответа - 200\n')
 
-print('\nЗапрос данных о питомце с сервера по id - ' + str(pet_id))
+print('Запрос данных о питомце с сервера по id - ' + str(pet_id))
 
 # GET-запрос
 res_3 = requests.get(f'https://petstore.swagger.io/v2/pet/{pet_id}', headers=headers)
 
 print(res_3.status_code)
-print(f'\nНа сервере имеются данные о питомце\nКод ответа - 200 ')
+print(f'На сервере имеются данные о питомце\nКод ответа - 200\n')
 
-print('\nУдаление данных о питомце с сервера по id - ' + str(pet_id))
+print('Удаление данных о питомце с сервера по id - ' + str(pet_id))
 
 # DELETE-запрос
 res_4 = requests.delete(f'https://petstore.swagger.io/v2/pet/{pet_id}', headers=headers)
 
 print(res_4.status_code)
-print(f'\nС сервера удалены данные о питомце\nКод ответа - 200')
+print(f'С сервера удалены данные о питомце\nКод ответа - 200\n')
 
-print('\nПовторный запрос данных о питомце с сервера по id - ' + str(pet_id))
+print('Повторный запрос данных о питомце с сервера по id - ' + str(pet_id))
 
 # Повторный GET-запрос
 res_5 = requests.get(f'https://petstore.swagger.io/v2/pet/{pet_id}', headers=headers)
 
 print(res_5.status_code)
-print('\nНа сервере нет данных о питомце\nКод ответа - 404')
+print('На сервере нет данных о питомце\nКод ответа - 404')
